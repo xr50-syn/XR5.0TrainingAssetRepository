bdbca3eb893b0a16f99d73dba88273ba18d1a764
emaurog@synelixis.com <emaurog@synelixis.com>
2024-07-22 14:16:48 +0300

    Emmie: hacked Amy's library

--- Controllers/XR50AppController.cs
+++ Controllers/XR50AppController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
 using XR5_0TrainingRepo.Models;
 
 namespace XR5_0TrainingRepo.Controllers
@@ -15,15 +19,35 @@ namespace XR5_0TrainingRepo.Controllers
     {
         private readonly XR50AppContext _context;
         private readonly HttpClient _httpClient;
-        public XR50AppController(XR50AppContext context)
+        public XR50AppController(XR50AppContext context, HttpClient httpClient)
         {
             _context = context;
+            _httpClient = httpClient;
         }
 
         // GET: api/XR50App
         [HttpGet]
         public async Task<ActionResult<IEnumerable<XR50App>>> GetApps()
         {
+
+            var values = new List<KeyValuePair<string, string>>();
+            FormUrlEncodedContent messageContent = new FormUrlEncodedContent(values);
+            string username = "emmie";
+            string password = "!@m!nL0v3W!th@my";
+                                
+            string authenticationString = $"{username}:{password}";
+            var base64EncodedAuthenticationString = Convert.ToBase64String(Encoding.ASCII.GetBytes(authenticationString));
+            
+            var request = new HttpRequestMessage(HttpMethod.Get, "")
+            {
+                Content = messageContent
+            };
+            request.Headers.Authorization = new AuthenticationHeaderValue("Basic", base64EncodedAuthenticationString);
+            _httpClient.BaseAddress = new Uri("http://192.168.169.6:8080/ocs/v1.php/cloud/groups");
+            _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"Basic {base64EncodedAuthenticationString}");
+            var result = _httpClient.SendAsync(request).Result;
+            string resultContent = result.Content.ReadAsStringAsync().Result;
+            Console.WriteLine($"Response content: {resultContent}");
             return await _context.Apps.ToListAsync();
         }
 
@@ -77,12 +101,29 @@ namespace XR5_0TrainingRepo.Controllers
         [HttpPost]
         public async Task<ActionResult<XR50App>> PostXR50App(XR50App xR50App)
         {
-            _context.Apps.Add(xR50App);
-            await _context.SaveChangesAsync();
 
+            _context.Apps.Add(xR50App);
+
+            await _context.SaveChangesAsync();
+            var values = new List<KeyValuePair<string, string>>();
+            values.Add(new KeyValuePair<string, string>("groupid", xR50App.OwncloudGroup));
+            FormUrlEncodedContent messageContent = new FormUrlEncodedContent(values);
+            string username = "emmie";
+            string password = "!@m!nL0v3W!th@my";
+            string authenticationString = $"{username}:{password}";
+            var base64EncodedAuthenticationString = Convert.ToBase64String(Encoding.ASCII.GetBytes(authenticationString));
+            var request = new HttpRequestMessage(HttpMethod.Post, "")
+            {
+                Content = messageContent
+            };
+            _httpClient.BaseAddress = new Uri("http://192.168.169.6:8080/ocs/v1.php/cloud/groups");
+            _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"Basic {base64EncodedAuthenticationString}");
+            var result = _httpClient.SendAsync(request).Result;
+            string resultContent = result.Content.ReadAsStringAsync().Result;
+            Console.WriteLine($"Response content: {resultContent}");
             return CreatedAtAction("GetXR50App", new { id = xR50App.AppId }, xR50App);
         }
-
+        
         // DELETE: api/XR50App/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteXR50App(long id)
