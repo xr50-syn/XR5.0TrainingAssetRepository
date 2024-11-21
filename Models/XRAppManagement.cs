699dd6bfe3b7873b71a7c8df3b48fa41588b53c5
Emm.N. Black <emaurog@synelixis.com>
2024-11-21 14:26:39 +0000

    Emmie: New Keys!

--- Models/XRAppManagement.cs
+++ Models/XRAppManagement.cs
@@ -1,21 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using XR5_0TrainingRepo.Models;
 namespace XR5_0TrainingRepo.Models
 {
     public class XR50App
     { 
-        
+         [Key]
+        public string AppName { get; set; }
+	[ForeignKey("Trainings")]
+        public virtual List<string>? TrainingList { get; set; }
+
         public string? OwncloudGroup { get; set; }
         public string? Description { get; set; }
         public string? OwncloudDirectory {  get; set; }
         public string? AdminName { get ; set; }
         public User AdminUser { get; set; }
-       
-        public List<TrainingModule>? TrainingList { get; set; }
-
-        [Key]
-        public string AppName { get; set; }
         
         public XR50App(){ }
 
