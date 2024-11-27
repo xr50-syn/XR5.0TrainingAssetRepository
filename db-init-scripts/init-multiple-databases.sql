552bac9f738da93d37325ddb0a09a5a1ee5cc309
tsanakas <tsanakas@synelixis.com>
2024-11-27 16:15:44 +0200

    removed somemariadb privileges from owncloud user

--- db-init-scripts/init-multiple-databases.sql
+++ db-init-scripts/init-multiple-databases.sql
@@ -2,7 +2,6 @@
 CREATE DATABASE IF NOT EXISTS some_mariadb;
 CREATE USER IF NOT EXISTS 'XR50user'@'%' IDENTIFIED BY 'my-secret-pw';
 GRANT ALL PRIVILEGES ON some_mariadb.* TO 'XR50user'@'%';
-GRANT ALL PRIVILEGES ON some_mariadb.* TO 'owncloud'@'%';
 
 -- Apply changes
 FLUSH PRIVILEGES;
