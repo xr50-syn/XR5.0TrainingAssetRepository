83df074988377c9495158e82e3bc879474ca9db7
tsanakas <tsanakas@synelixis.com>
2024-12-02 11:58:03 +0200

    dockerization v0.9: only thing missing is handling existing migration files

--- db-init-scripts/init-multiple-databases.sql
+++
@@ -1,7 +0,0 @@
--- Create the first database and user
-CREATE DATABASE IF NOT EXISTS some_mariadb;
-CREATE USER IF NOT EXISTS 'XR50user'@'%' IDENTIFIED BY 'my-secret-pw';
-GRANT ALL PRIVILEGES ON some_mariadb.* TO 'XR50user'@'%';
-
--- Apply changes
-FLUSH PRIVILEGES;
--- run-migrations.sh
+++ run-migrations.sh
@@ -4,7 +4,7 @@ set -e
 echo "Waiting for database to be ready..."
 
 # Wait for MySQL to be ready
-until mysql -h mariadb -uXR50user -pmy-secret-pw -e "SELECT 1;" > /dev/null 2>&1; do
+until mysql -h mariadb -u${XR50_REPO_DB_USER} -p${XR50_REPO_DB_PASSWORD} -e "SELECT 1;" > /dev/null 2>&1; do
     echo "MySQL is unavailable - sleeping"
     sleep 5
 done
@@ -12,7 +12,7 @@ done
 echo "MySQL is up - running migrations..."
 
 # Generate migrations if not already generated (not recommended for production)
-if [ ! -d "./Migrations" ]; then
+if [ ! -d "./Migrations" ]; then #TODO FIND A BETTER WAY!!!!!!
   echo "Migrations directory not found. Generating initial migration..."
   dotnet ef migrations add InitialCreate
   # Apply migrations
