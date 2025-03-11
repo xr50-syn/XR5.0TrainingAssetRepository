0620b7b466357c13658bdd99f7f68b977ea9fd87
emaurog@synelixis.com <emaurog@synelixis.com>
2025-03-11 10:33:16 +0200

    Wizard: Correct Professor's Wife's meddling with Arcane Spells

--- Models/Material.cs
+++ Models/Material.cs
@@ -3,28 +3,39 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using XR5_0TrainingRepo.Models;
 
-namespace XR5_0TrainingRepo.Models {
+namespace XR5_0TrainingRepo.Models
+{
     public class Material
     {
         public string? TennantName { get; set; }
         public string? Description { get; set; }
         public string? OwncloudFileName { get; set; }
         public string? TrainingName { get; set; }
-        public string? ParentId {get; set;}
-        public string? ParentType {get; set;}
+        public string? ParentId { get; set; }
+        public string? ParentType { get; set; }
         public string? MaterialName { get; set; }
-	    [ForeignKey("Assets")]
+        [ForeignKey("Assets")]
         public virtual List<string>? AssetList { get; set; }
         [ForeignKey("Materials")]
-        public virtual List<string>? MaterialList {get;set;}
-	    [Key]
-        public string? MaterialId {get; set;}
+        public virtual List<string>? MaterialList { get; set; }
+        [Key]
+        public string? MaterialId { get; set; }
+
+        [Required]
+        public MaterialType MaterialType { get; set; }
 
         public Material()
         {
-	        MaterialId= Guid.NewGuid().ToString();
+            MaterialId = Guid.NewGuid().ToString();
             MaterialList = new List<string>();
             AssetList = new List<string>();
         }
+    }
+    public enum MaterialType
+    {
+        Checklist,
+        Image,
+        Video,
+        Workflow
     }
 }
